ee87fe25baf3b1b1f0f969ccafc112f05ecef41e
NavinAnik <smnavinnayeranik@gmail.com>
2023-11-09 16:08:55 +0600

    refactor(postController, postRoutes): reafctor the code based on funtionalities

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,16 +1,29 @@
 const fs = require("fs");
+const errorHandler = require("../utils/errorHandler");
+const bodyParser = require("../utils/bodyParser");
 
-module.exports = async function postController(req, body) {
-  try {
-    body.id = Math.floor(Math.random() * 1000000).toString();
-    req.data.push(body);
-    fs.writeFile("./dataModel/data.json", JSON.stringify(req.data), (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log("File written successfully");
-    });
-  } catch (err) {
-    console.log(err, "File not found");
-  }
+module.exports = async function postController(req, res) {
+  const body = await bodyParser(req);
+  if (
+    body.name !== "" ||
+    body.username !== "" ||
+    body.email !== "" ||
+    body.name !== undefined ||
+    body.username !== undefined ||
+    body.email !== undefined
+  )
+    try {
+      body.id = Math.floor(Math.random() * 1000000).toString();
+      req.data.push(body);
+      fs.writeFile("./dataModel/data.json", JSON.stringify(req.data), (err) => {
+        if (err) {
+          errorHandler(req, res);
+        }
+        res.statusCode = 201;
+        res.write("Employee created successfully");
+        res.end();
+      });
+    } catch (err) {
+      errorHandler(req, res);
+    }
 };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,31 +1,11 @@
 const postController = require("../controllers/postController");
-const bodyParser = require("../utils/bodyParser");
+const errorHandler = require("../utils/errorHandler");
 module.exports = async function postRoutes(req, res) {
   if (req.url === "/api/employees/create") {
-    console.log("inside post");
-    res.setHeader("Content-Type", "text/html");
-    const body = await bodyParser(req);
-    if (
-      body.name !== "" &&
-      body.username !== "" &&
-      body.email !== "" &&
-      body.name !== undefined &&
-      body.username !== undefined &&
-      body.email !== undefined
-    ) {
-      try {
-        console.log("inside try");
-        await postController(req, body);
-        res.statusCode = 201;
-        res.write("Employee created successfully");
-        res.end();
-      } catch (err) {
-        res.writeHead(404);
-        res.end("Failed to create employee");
-      }
+    {
+      await postController(req, res);
     }
   } else {
-    res.writeHead(404);
-    res.end("Something went wrong");
+    errorHandler(req, res);
   }
 };
